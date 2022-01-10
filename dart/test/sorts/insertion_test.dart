455cc4f4169b1d320faea40a6ccf731675e3f1e0
youngmin-gwon <youngmin.gwon@gmail.com>
2022-01-10 21:56:23 +0900

    📰Feat: 추가 insertion sort init
    무엇을: insertion sort 파일 생성

---
+++ dart/test/sorts/insertion_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+
+void main() {
+  late List<int> randomList,
+      sortedRandomListAscending,
+      sortedRandomListDescending;
+
+  late List<String> emptyList;
+
+  setUp(() {});
+}

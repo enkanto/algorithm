59b56b552e411a12bc8757843fb1e93cd285dd8f
youngmin-gwon <youngmin.gwon@gmail.com>
2022-01-11 21:03:05 +0900

    📰Feat: 추가 binary sort
    무엇을: binary sort and test code 추가

--- cpp/testfile.cpp
+++ cpp/testfile.cpp
@@ -14,3 +14,25 @@ int main()
     }
     cout << endl;
 }
+
+// leetcode: 704
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while(left<=right){
+            int pivot = (left+right)>>1;
+            if (nums[pivot] == target){
+                return pivot;
+            } else if (nums[pivot] < target) {
+                left = pivot+1;
+            } else {
+                right = pivot-1;
+            }
+        }
+        
+        return -1;
+    }
+};
